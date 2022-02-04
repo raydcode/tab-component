7528acb077089f88c6507cda2aceea12afcd2e55
kathir2602 <kathir@crayond.co>
2022-02-04 21:07:42 +0530

    init - tabs

--- src/App.js
+++ src/App.js
@@ -1,9 +1,66 @@
-import './App.css';
+import React, { useState } from 'react';
+import { FaAngleDoubleRight } from 'react-icons/fa';
+
+import { data } from './data';
 
 function App() {
-  return <div className="App">
-    
-  </div>;
+  const [loading, setLoading] = useState(true);
+  const [jobs, setJobs] = useState(data);
+  const [value, setValue] = useState(0);
+
+  setTimeout(() => {
+    setLoading(false);
+  }, 2000);
+
+  if (loading) {
+    return (
+      <section className="section loading">
+        <h1>Loading...</h1>
+      </section>
+    );
+  }
+  const { company, dates, duties, title } = jobs[value];
+  return (
+    <section className="section">
+      <div className="title">
+        <h2>experience</h2>
+        <div className="underline"></div>
+      </div>
+      <div className="jobs-center">
+        {/* btn container */}
+        <div className="btn-container">
+          {jobs.map((item, index) => {
+            return (
+              <button
+                key={item.id}
+                onClick={() => setValue(index)}
+                className={`job-btn ${index === value && 'active-btn'}`}
+              >
+                {item.company}
+              </button>
+            );
+          })}
+        </div>
+        {/* job info */}
+        <article className="job-info">
+          <h3>{title}</h3>
+          <h4>{company}</h4>
+          <p className="job-date">{dates}</p>
+          {duties.map((duty, index) => {
+            return (
+              <div key={index} className="job-desc">
+                <FaAngleDoubleRight className="job-icon"></FaAngleDoubleRight>
+                <p>{duty}</p>
+              </div>
+            );
+          })}
+        </article>
+      </div>
+      <button type="button" className="btn">
+        more info
+      </button>
+    </section>
+  );
 }
 
 export default App;
